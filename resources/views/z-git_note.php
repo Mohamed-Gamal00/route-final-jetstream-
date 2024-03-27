dea790ed873a94fdaa73b34176dc867596c5e84b
Mohamed-Gamal00 <gmohamed777@yahoo.com>
2024-03-27 13:40:17 +0200

    first commit

---
+++ resources/views/z-git_note.php
@@ -0,0 +1,17 @@
+<?php
+/*
+create branch
+
+- check branches                      $ git branch
+
+- last comment on branch              $ git branch -v
+
+- create branch                       $ git branch branch-name
+
+- delete barnch                       $ git branch -d branch-name
+
+- switch to branch                    $ git checkout branch-name  or git switch branch-name
+
+- Everything up-to-date               $ git push --all origin
+*/
+?>
