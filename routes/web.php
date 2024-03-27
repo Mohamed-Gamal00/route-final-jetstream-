5e5f375936c158701268761c40c5fd45a7c29ef1
Mohamed-Gamal00 <gmohamed777@yahoo.com>
2024-03-27 14:41:32 +0200

    home controller to user and admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +27,7 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('redirect',[HomeController::class,'redirect']);
+// Route::get('/home', [HomeController::class, 'index'])->name('home');
+
